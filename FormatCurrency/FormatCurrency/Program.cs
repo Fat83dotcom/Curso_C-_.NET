b3c8260bd74858b6af36000cc72b0fb8294d2562
fat83dotcom <fernando.quimic@gmail.com>
2025-02-10 15:41:52 -0300

    Metodos para formatação de decimais

---
+++ FormatCurrency/FormatCurrency/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+decimal value = 66.52m;
+
+// Modifica a forma que o ponto decimal é mostrado
+CultureInfo culture = CultureInfo.CreateSpecificCulture("pt-BR");
+Console.WriteLine(value.ToString("C", culture)); // o formatador "C" ja coloca o tipo da moeda
+// formatadores para decimal
+// G => Generico
+// F => Maior Precisão
+// N => Número sem formataçaõ de moeda
+// P => Porcentagem
+
+Console.WriteLine(Math.Round(value, 1)); // arredonda o numero.
+Console.WriteLine(Math.Ceiling(value)); // arredonda para cima
+Console.WriteLine(Math.Floor(value)); // arredonda para baixo
+
