958ff4bd80741ec07d5e1f51c728d2503785d450
fat83dotcom <fernando.quimic@gmail.com>
2025-02-10 12:19:52 -0300

    Arquivos de compilação.

--- EditorHTML/EditorHTML/obj/Debug/net9.0/EditorHTML.AssemblyInfo.cs
+++ EditorHTML/EditorHTML/obj/Debug/net9.0/EditorHTML.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EditorHTML")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa535b82f0e76ebcd96477bc90fd2a502c355bc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64a4c05ba1e6917b4c0765bceaf8d6e21d4052fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("EditorHTML")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EditorHTML")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
