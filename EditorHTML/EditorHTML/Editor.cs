64a4c05ba1e6917b4c0765bceaf8d6e21d4052fe
fat83dotcom <fernando.quimic@gmail.com>
2025-02-09 21:23:19 -0300

    Novos comits

---
+++ EditorHTML/EditorHTML/Editor.cs
@@ -0,0 +1,70 @@
+﻿
+using System.Text;
+
+namespace EditorHTML
+{
+    public static class Editor
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("Modo Editor");
+            Console.WriteLine("-----------");
+            Start();
+        }
+
+        public static void Start()
+        {
+            StringBuilder file = new();
+
+            do
+            {
+                file.Append(Console.ReadLine());
+                file.Append(Environment.NewLine);
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Console.WriteLine("-----------");
+            Console.WriteLine("_Deseja salvar o arquivo?[S/N]");
+            var option = Console.ReadLine();
+            if (option != null && option.Length == 1)
+            {
+                option = option.ToUpper();
+                SaveFile(option, file);
+            }
+
+        }
+
+        public static void SaveFile(string option, StringBuilder file)
+        {
+            if (option == "S")
+            {
+                try
+                {
+                    string nameFile =  Guid.NewGuid().ToString();
+
+                    string pathFile = $"C:\\Users\\ferna\\OneDrive\\Área de Trabalho\\{nameFile}.txt";
+
+                    using StreamWriter writer = new(pathFile);
+                    {
+                        writer.Write(file.ToString());
+                    }
+                    Console.WriteLine("Arquivo salvo com sucesso.");
+                    Thread.Sleep(3000);
+                    Viewer.Show(file.ToString());
+                }
+                catch
+                {
+                    Console.WriteLine("Erro: Não foi possovel salvar o arquivo.");
+                }
+            }
+            else
+            {
+                Menu.Show();
+            }
+        }
+    }
+}
---
+++ EditorHTML/EditorHTML/Menu.cs
@@ -0,0 +1,99 @@
+﻿
+namespace EditorHTML
+{
+    public static class Menu
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+            DrawScreen();
+            WriteOptions();
+
+            var str = Console.ReadLine();
+            short option = short.Parse(str);
+            HandleMenuOptions(option);
+
+        }
+
+        internal static void DrawUpAndDownLines(int width)
+        {
+            Console.Write('+');
+            for (int i = 0; i <= width; i++)
+            {
+                Console.Write('-');
+            }
+
+            Console.Write('+');
+            Console.Write('\n');
+        }
+
+        internal static void DrawnBodyLines(int height)
+        {
+            for (int lines = 0; lines < height; lines++)
+            {
+                Console.Write('|');
+                for (int i = 0; i <= 30; i++)
+                {
+                    Console.Write(' ');
+                }
+                Console.Write('|');
+                Console.Write('\n');
+            }
+        }
+         
+        public static void DrawScreen()
+        {
+            DrawUpAndDownLines(30);
+            DrawnBodyLines(10);
+            DrawUpAndDownLines(30);
+        }
+
+        public static void WriteOptions()
+        {
+            Console.SetCursorPosition(10, 2);
+            Console.Write("Editor Html");
+
+            Console.SetCursorPosition(10, 3);
+            Console.Write("===========");
+            
+            Console.SetCursorPosition(3, 4);
+            Console.Write("Selecione uma opção abaixo:");
+                
+            Console.SetCursorPosition(3, 5);
+            Console.Write("1 - Novo Arquivo");
+
+            Console.SetCursorPosition(3, 6);
+            Console.Write("2 - Abrir arquivo");
+
+            Console.SetCursorPosition(3, 7);
+            Console.Write("3 - Sair");
+            
+            Console.SetCursorPosition(3, 8);
+            Console.Write("Opção: ");
+
+            Console.SetCursorPosition(10, 8);
+        }
+
+        public static void HandleMenuOptions(short option)
+        {
+            switch(option)
+            {
+                case 1: Editor.Show();
+                    break;
+                case 2: Console.WriteLine("View");
+                    break;
+                case 3:
+                    {
+                        Console.Clear();
+                        Environment.Exit(0);
+                        break;
+                    }
+                default:
+                    Show();
+                    break;
+            }
+        }
+    }
+}
