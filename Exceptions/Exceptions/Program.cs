33204eb7a548cbe1ebb1648d3479413344f2b759
fat83dotcom <fernando.quimic@gmail.com>
2025-02-10 20:48:18 -0300

    Adicionando os comandos basicos do try catch:

---
+++ Exceptions/Exceptions/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exceptionss
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int[] arr = [1, 2, 3];
+
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message.ToString());
+                // exibe uma mensagem mais amigavel para o usuario.i
+            }
+        }
+    }
+}
