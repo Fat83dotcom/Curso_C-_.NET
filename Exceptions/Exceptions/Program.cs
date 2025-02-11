560cf16a31a3d340ba7f1dd6419ec97249572bf3
fat83dotcom <fernando.quimic@gmail.com>
2025-02-10 22:02:04 -0300

    Adicionando estudo de try catch

--- Exceptions/Exceptions/Program.cs
+++ Exceptions/Exceptions/Program.cs
@@ -8,15 +8,36 @@
 
             try
             {
+                Saver("");
                 for (int i = 0; i < 10; i++)
                 {
                     Console.WriteLine(arr[i]);
                 }
             }
+            catch(IndexOutOfRangeException e)
+            {
+                // Os erros são tratados dos mais especificos para os mais genericos
+                Console.WriteLine(e.Message.ToString());
+                Console.WriteLine("Não foi possivel encontrar o indice");
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine(e.Message.ToString());
+                // captura o erro lançado pelo throw
+            }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message.ToString());
-                // exibe uma mensagem mais amigavel para o usuario.i
+                // exibe uma mensagem mais amigavel para o usuario.
+            }
+        }
+
+        private static void Saver(string filename)
+        {
+            if (string.IsNullOrEmpty(filename))
+            {
+                // lança um erro quando o arquivo é vazio ou nulo
+                throw new ArgumentNullException("O arquivo não pode ser vazio ou nulo.");
             }
         }
     }
