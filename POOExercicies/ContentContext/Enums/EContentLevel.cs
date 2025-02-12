78f38d34ba5c09e69bbc2f9161dbaabe183056f3
fat83dotcom <fernando.quimic@gmail.com>
2025-02-12 12:53:01 -0300

    retirando importações nao usadas

--- POOExercicies/ContentContext/Enums/EContentLevel.cs
+++ POOExercicies/ContentContext/Enums/EContentLevel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace POOExercicies.ContentContext.Enums
+﻿namespace POOExercicies.ContentContext.Enums
 {
     public enum EContentLevel
     {
