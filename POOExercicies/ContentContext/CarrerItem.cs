1a7d14718dc32618c6897de2642913016ee5351e
fat83dotcom <fernando.quimic@gmail.com>
2025-02-12 16:04:27 -0300

    Adicionando uma verifição no setter

--- POOExercicies/ContentContext/CarrerItem.cs
+++ POOExercicies/ContentContext/CarrerItem.cs
@@ -1,10 +1,34 @@
 ﻿namespace POOExercicies.ContentContext
 {
-    public class CarrerItem(int order, string? title, string? description, Course? course)
+    public class CarrerItem
     {
-        public int Order { get; set; } = order;
-        public string? Title { get; set; } = title;
-        public string? Description { get; set; } = description;
-        public Course? Course { get; set; } = course;
+        public CarrerItem(int order, string? title, string? description, Course course)
+        {
+            Order = order;
+            Title = title;
+            Description = description;
+            Course = course;
+        }
+
+        public int Order { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+            
+        public Course? Course
+        {
+            get;
+            set
+            {
+                if (value != null)
+                {
+                    Console.WriteLine("passou aqui");
+                    field = value;
+                }
+                else
+                {
+                    throw new ArgumentNullException(nameof(value), "O curso não pode ser nulo.");
+                }
+            }
+        }
     }
 }
