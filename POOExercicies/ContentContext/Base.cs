5244710331c6a6602d09dd08f40b00ba61285514
fat83dotcom <fernando.quimic@gmail.com>
2025-02-12 17:40:20 -0300

    Criando a classe Base.

---
+++ POOExercicies/ContentContext/Base.cs
@@ -0,0 +1,10 @@
+﻿
+using POOExercicies.NotificationContext;
+
+namespace POOExercicies.ContentContext
+{
+    public abstract class Base : Notifiable
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+    }
+}
