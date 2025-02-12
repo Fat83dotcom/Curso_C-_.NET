938120dd813540689eadcc5c74b0c3017bae3d4f
fat83dotcom <fernando.quimic@gmail.com>
2025-02-12 12:49:21 -0300

    retirando importações não usadas

--- POOExercicies/ContentContext/CarrerItem.cs
+++ POOExercicies/ContentContext/CarrerItem.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace POOExercicies.ContentContext
+﻿namespace POOExercicies.ContentContext
 {
     public class CarrerItem(int order, string? title, string? description, Course? course)
     {
