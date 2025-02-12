2ce314c98b8489961c3f7110ded27668d66a13b3
fat83dotcom <fernando.quimic@gmail.com>
2025-02-12 12:47:52 -0300

    Removendo importações não usadas

--- POOExercicies/ContentContext/Carrer.cs
+++ POOExercicies/ContentContext/Carrer.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace POOExercicies.ContentContext
+﻿namespace POOExercicies.ContentContext
 {
     public class Carrer(string? title, string? url) : Content(title, url)
     {
