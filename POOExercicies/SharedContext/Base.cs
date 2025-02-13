d00c999208af4fb534869b41605b53b2b53ad99d
fat83dotcom <fernando.quimic@gmail.com>
2025-02-12 22:24:05 -0300

    Novo modulo para a classe base

---
+++ POOExercicies/SharedContext/Base.cs
@@ -0,0 +1,9 @@
+﻿using POOExercicies.NotificationContext;
+
+namespace POOExercicies.SharedContext
+{
+    public abstract class Base : Notifiable
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+    }
+}
