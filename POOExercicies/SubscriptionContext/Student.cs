f966737f3643889eca6cbd6e6a42cc5c73650c73
fat83dotcom <fernando.quimic@gmail.com>
2025-02-12 22:30:24 -0300

    feat: Implementa classe Student com gerenciamento de assinaturas
    Adiciona a classe Student com propriedades para armazenar informações do aluno (User, Name, Email) e uma lista de assinaturas (Subscriptions).
    
    Implementa a propriedade IsPremium para verificar se o aluno possui alguma assinatura ativa.
    
    Adiciona o método CreateSubscription para criar novas assinaturas, evitando duplicidade em caso de aluno já ser premium (adiciona uma notificação nesse cenário).

---
+++ POOExercicies/SubscriptionContext/Student.cs
@@ -0,0 +1,31 @@
+﻿
+using POOExercicies.SharedContext;
+
+namespace POOExercicies.SubscriptionContext
+{
+    public class Student : Base
+    {
+
+        public Student(User user, string? name, string? email)
+        {
+            User = user;
+            Name = name;
+            Email = email;
+        }
+        public User? User { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public IList<Subscription> Subscriptions  { get; set; } = [];
+        public bool IsPremium => Subscriptions.Any(x => x.IsActive);
+
+        public void CreateSubscription(Subscription subscription)
+        {
+            if (IsPremium)
+            {
+                AddNotification(new("Premium", "O aluno já é premium."));
+                return;
+            }
+            Subscriptions.Add(subscription);
+        }
+    }
+}
