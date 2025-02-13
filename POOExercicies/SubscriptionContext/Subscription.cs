6b455387e46c0fe37dfa3a72c4db6ce65f44de89
fat83dotcom <fernando.quimic@gmail.com>
2025-02-12 22:32:18 -0300

    feat: Implementa classe abstrata Subscription para gerenciar assinaturas
    Adiciona a classe abstrata Subscription com propriedades para armazenar o Plan (plano) e a EndDate (data de término) da assinatura.
    
    Implementa a propriedade IsActive para verificar se a assinatura está ativa com base na comparação da EndDate com a data atual.

---
+++ POOExercicies/SubscriptionContext/Subscription.cs
@@ -0,0 +1,12 @@
+﻿
+namespace POOExercicies.SubscriptionContext
+{
+    public abstract class Subscription
+    {
+        public required Plan Plan { get; set; }
+        public DateTime? EndDate { get; set; }
+
+        public bool IsActive => EndDate >= DateTime.Now;
+
+    }
+}
