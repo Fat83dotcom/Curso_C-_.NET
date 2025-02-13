fcb528ecb4c7e1d6a54cc7527dd3ca55a5f2ba74
fat83dotcom <fernando.quimic@gmail.com>
2025-02-12 22:34:10 -0300

    feat: Implementa classe User para gerenciar informações de usuário
    Adiciona a classe User com propriedades para armazenar o nome de usuário (UserName) e senha (Password).
    
    Herda da classe Base para compartilhar funcionalidades comuns, como identificação única.

---
+++ POOExercicies/SubscriptionContext/User.cs
@@ -0,0 +1,11 @@
+﻿
+using POOExercicies.SharedContext;
+
+namespace POOExercicies.SubscriptionContext
+{
+    public class User : Base
+    {
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+    }
+}
